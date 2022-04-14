34974bca58739ede838fd458c42970ae0cb5581b
realhsb <realhsb@gmail.com>
2022-04-14 16:33:33 +0900

    #87389 - 나머지가 1이 되는 수 찾기

---
+++ Programmers/src/level1/Solution_87389.java
@@ -0,0 +1,14 @@
+package level1;
+
+//#87389 - 나머지가 1이 되는 수 찾기 
+
+class Solution_87389 {
+    public int solution(int n) {
+        for(int i = 2; i < n; i++){
+            if(n % i == 1){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
