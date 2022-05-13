c269654ebff7f90d294bc3e8c9732a246c7a6487
realhsb <realhsb@gmail.com>
2022-05-13 16:30:34 +0900

    좋은 수

---
+++ JAVA-DoitCodingTest/src/자료구조/P1253_좋은수.java
@@ -0,0 +1,45 @@
+package 자료구조;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class P1253_좋은수 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(bf.readLine());
+		int Result = 0;
+		long[] A = new long[N];
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		for(int i = 0; i < N; i++) {
+			A[i] = Long.parseLong(st.nextToken());
+		}
+		Arrays.sort(A);
+		for(int k = 0; k < N; k++) {
+			long find = A[k];
+			int i = 0;
+			int j = N - 1;
+			while(i < j) {
+				if(A[i] + A[j] == find) {
+				// find는 서로 다른 두 수의 합이어야 함을 체크 
+					if(i != k && j != k) {
+						Result++;
+						break;
+					}else if(i == k) {
+						i++;
+					}else if(j == k) {
+						j--;
+					}
+				} else if(A[i] + A[j] < find) {
+					i++;
+				} else {
+					j--;
+				}
+			}
+		}
+		System.out.println(Result);
+		bf.close();
+	}
+}
