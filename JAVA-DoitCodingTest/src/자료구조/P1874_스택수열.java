26a43185a03ae9179cefb4adebaf7792130a9833
realhsb <realhsb@gmail.com>
2022-05-22 21:46:43 +0900

    P1874 스택 수열

--- JAVA-DoitCodingTest/src/자료구조/P1874_스택수열.java
+++ JAVA-DoitCodingTest/src/자료구조/P1874_스택수열.java
@@ -18,17 +18,18 @@ public class P1874_스택수열 {
 		boolean result = true;
 		
 		for(int i = 0; i < A.length; i++) {
-			int su = A[i];
-			if(su >= num) {
-				while(su >= num) {
+			int su = A[i];					// 현재 수열의 수
+			if(su >= num) {					// 현재 수열의 값 >= 오름차순 자연수 : 값이 같아질 때까지 push() 수행 
+				while(su >= num) {			// push() 
 					stack.push(num++);
 					bf.append("+\n");
 				}
 				stack.pop();
 				bf.append("-\n");
-			}else {
+			}else {							// 현재 수열 값 < 오름차순 자연수 : pop()을 수행해 수열 원소를 꺼냄 
 				int n = stack.pop();
 				
+				// 스택의 가장 위의 수가 만들어야 하는 수열의 수보다 크면 수열을 출력할 수 없음 
 				if(n > su) {
 					System.out.println("NO");
 					result = false;
