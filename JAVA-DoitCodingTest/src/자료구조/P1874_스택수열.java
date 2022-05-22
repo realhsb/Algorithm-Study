40da763096a2bc4d69209b1d4eaab72945f27799
realhsb <realhsb@gmail.com>
2022-05-22 21:08:27 +0900

    P1874 스택 수열

---
+++ JAVA-DoitCodingTest/src/자료구조/P1874_스택수열.java
@@ -0,0 +1,83 @@
+package 자료구조;
+
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.Stack;
+
+public class P1874_스택수열 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] A = new int[N];
+		for (int i = 0; i < N; i++) {
+			A[i] = sc.nextInt();
+		}
+		Stack<Integer> stack = new Stack<>();
+		StringBuffer bf = new StringBuffer();
+		int num = 1;
+		boolean result = true;
+		
+		for(int i = 0; i < A.length; i++) {
+			int su = A[i];
+			if(su >= num) {
+				while(su >= num) {
+					stack.push(num++);
+					bf.append("+\n");
+				}
+				stack.pop();
+				bf.append("-\n");
+			}else {
+				int n = stack.pop();
+				
+				if(n > su) {
+					System.out.println("NO");
+					result = false;
+					break;
+				} else {
+					bf.append("-\n");
+				}
+			}
+		}
+		if(result) System.out.println(bf.toString());
+	}
+}
+
+//예제 입력 1
+//8
+//4
+//3
+//6
+//8
+//7
+//5
+//2
+//1
+
+//예제 출력 1
+//+
+//+
+//+
+//+
+//-
+//-
+//+
+//+
+//-
+//+
+//+
+//-
+//-
+//-
+//-
+//-
+
+//예제 입력 2
+//5
+//1
+//2
+//5
+//3
+//4
+
+//예제 출력 2
+//NO
